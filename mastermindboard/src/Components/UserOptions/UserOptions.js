588afd5974d589ffa1bcdd4ce2ea20c8d31ed3d0
arvind8998 <asangwan8998@gmail.com>
2021-06-29 17:34:38 +0530

    Added selected color

--- mastermindboard/src/Components/UserOptions/UserOptions.js
+++ mastermindboard/src/Components/UserOptions/UserOptions.js
@@ -3,6 +3,7 @@ import "./UserOptions.css"
 
 const UserOptions = () => {
   let [options, setOptions] = useState([])
+  let [selectedColour, setSelectedColor] = useState("")
   useEffect(() => {
     let colourOptions = [
       { 0: "yellow" },
@@ -14,11 +15,24 @@ const UserOptions = () => {
     ]
     setOptions(colourOptions)
   }, [])
+
+  let handleUserSelect = (e) => {
+    setSelectedColor(e.target.className)
+
+  }
   return (
     <div>
       <div className="user__options">
         {options.map((obj, idx) => (
-          <div key={"id" + idx} className={`option ${obj[idx]}`}></div>
+          <div
+            onClick={handleUserSelect}
+            key={"id" + idx}
+            className={`${
+              selectedColour === `option ${obj[idx]}`
+                ? `option ${obj[idx]} selected`
+                : `option ${obj[idx]}`
+            }`}
+          ></div>
         ))}
       </div>
     </div>
